1e08b576aa6f185f31c2c776a6baf2e90bc6a693
davegb313 <davidbesnovatiy@gmail.com>
2019-07-26 13:07:10 +0300

    change number of digits fn

--- src/App.js
+++ src/App.js
@@ -6,54 +6,73 @@ class App extends React.Component {
 
   state = {
     numbers: [0,0,0,0],
-    secretCode: 1111,
+    secretCode: '1111',
   }
 
-changeValue = (index, diff) => {
-  this.setState({
+changeNumOfDigits = (diff)=> {
+  let newNumbers = []
+  let newSecretCode = ''
+    if (diff > 0) {
+      newNumbers =  this.state.numbers.concat(0);
+      newSecretCode = this.state.secretCode + '1';
+    } else {
+      newNumbers = this.state.numbers.slice(0, -1);
+      newSecretCode = this.state.secretCode.slice(0, -1);
+    }
+    this.setState({
+      numbers: newNumbers,
+      secretCode: newSecretCode,
+    })
+  }
+
+scrollBot = ()=> (window.scrollTo({
+    top: (window.innerHeight), behavior: "smooth"
+  }));
+
+scrollTop = ()=> (window.scrollTo({
+    top: (0), behavior: "smooth"
+  }));
+
+changeValue = (index, diff) => this.setState({
     numbers: this.state.numbers.map(( value, i )=>
-      (i === index) ? (value + diff +10) % 10 : value
-    )
-  })
-}
+      (i === index) ? (value + diff +10) % 10 : value)
+  });
 
 check = ()=> {
-  const userCode = parseInt(this.state.numbers[0].toString()+this.state.numbers[1].toString()+
-                            this.state.numbers[2].toString()+this.state.numbers[3].toString());
+  const userCode = this.state.numbers.reduce((p, c)=>p+c.toString() ,'');
   userCode === this.state.secretCode ?
     this.setState({ isOpen: true, }) : this.setState({ isOpen: false, })
-}
+};
 
 
   render () {
-    console.log(this.state);
     return (
       <div className="App">
-        <h1>The Lock</h1>
-        <Lock opened={this.state.isOpen}/>
-        <div className='numbers'>
-          <div className='number'>
-            <button className='button' onClick={()=> this.changeValue(0, 1)}>up</button>
-            <span className='btncd'>{this.state.numbers[0]}</span>
-            <button className='button' onClick={()=> this.changeValue(0, -1)}>dn</button>
-          </div>
-          <div className='number'>
-            <button className='button' onClick={()=> this.changeValue(1, 1)}>up</button>
-            <span className='btncd'>{this.state.numbers[1]}</span>
-            <button className='button' onClick={()=> this.changeValue(1, -1)}>dn</button>
-          </div>
-          <div className='number'>
-            <button className='button' onClick={()=> this.changeValue(2, 1)}>up</button>
-            <span className='btncd'>{this.state.numbers[2]}</span>
-            <button className='button' onClick={()=> this.changeValue(2, -1)}>dn</button>
-          </div>
-          <div className='number'>
-            <button className='button' onClick={()=> this.changeValue(3, 1)}>up</button>
-            <span className='btncd'>{this.state.numbers[3]}</span>
-            <button className='button' onClick={()=> this.changeValue(3, -1)}>dn</button>
-          </div>
+        <div className='chooseLock'>
+          <h1>The Lock</h1>
+          <p>How much digits on The Lock do you want?</p>
+          <button className='changeButtons' onClick={()=> this.changeNumOfDigits(1)}>more</button>
+          <span className='codes'>{this.state.numbers.length}</span>
+          <button className='changeButtons' onClick={()=> this.changeNumOfDigits(-1)}>less</button>
+          <button className='changeButtons' onClick={()=> this.scrollBot()}>gooo..</button>
         </div>
-        <button className='button' onClick={this.check}>CHECK</button>
+        <div className='theLock'>
+          <h1>The Lock</h1>
+          <Lock opened={this.state.isOpen}/>
+          <div className='numbers'>
+            {[...Array(this.state.numbers.length)].map((digit, i)=> (
+              <div className='number'>
+                <button className='changeButtons' onClick={()=> this.changeValue(i, 1)}>up</button>
+                <span className='codes'>{this.state.numbers[i]}</span>
+                <button className='changeButtons' onClick={()=> this.changeValue(i, -1)}>dn</button>
+              </div>
+            ))}
+          </div>
+          <button className='check' onClick={this.check}>CHECK</button>
+          <button className='toTheTop' onClick={()=> this.scrollTop()}>
+            <img src='https://chistokrov.ru/assets/images/arrow-top.png' alt='tothetop' />
+          </button>
+        </div>
       </div>
     );
   }
